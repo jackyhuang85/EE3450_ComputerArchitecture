3ce8f4ab76face308e48629655f547d4e373a67d
jackyhuang85 <jackyhuang85@gmail.com>
2017-10-28 22:58:49 +0800

    Build PA2 and finish it

--- pa2/src/main/scala/HammingCode.scala
+++ pa2/src/main/scala/HammingCode.scala
@@ -12,18 +12,18 @@ class HammingCode extends Module {
   val p2 = io.in(3) ^ io.in(2) ^ io.in(0)
   val p1 = io.in(3) ^ io.in(1) ^ io.in(0)
   val hamming = Reg(UInt(7.W))
-  val valid_reg = Reg()
-  when(enable) {
-    hamming = Cat(io.in(3,1), p3, io.in(0), p2, p1)
-    valid_reg = 1.W
+  val valid_reg = Reg(UInt())
+  when(io.enable) {
+    hamming := Cat(io.in(3,1), p3, io.in(0), p2, p1)
+    valid_reg := 1.U
   }
   .otherwise {
-    hamming = hamming
-    valid_reg = 0.W
+    hamming := hamming
+    valid_reg := 0.U
   }
 
-  out := hamming
-  valid := valid_reg
+  io.out := hamming
+  io.valid := valid_reg
 
 }
 
